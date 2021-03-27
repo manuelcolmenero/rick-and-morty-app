ee144bdc5468da7b94bc5cce30630a9af833a5da
Manuel Colmenero Navarro <mcolmenero@nubico.es>
2021-03-27 18:37:39 +0100

    Add basic functionality to Home

--- rickandmortyapp/Features/Home/HomeViewController.swift
+++ rickandmortyapp/Features/Home/HomeViewController.swift
@@ -6,3 +6,115 @@
 //
 
 import Foundation
+import RxSwift
+
+class HomeViewController: UIViewController {
+    // MARK: - Properties
+    private let viewModel = HomeViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private static let loadStartAt = 5
+    private var isLoadingCharacters: Bool = false
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureObservers()
+        configureView()
+        
+        viewModel.loadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.title = "Characters" // TODO: Change hardcode
+    }
+    
+    // MARK: - Configure methods
+    private func configureView() {
+        configureTableView()
+    }
+    
+    // MARK: - Observers functions
+    private func configureObservers() {
+        viewModel.needReloadData
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                self?.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Reaload Data
+    private func reloadData() {
+        isLoadingCharacters = false
+        tableView.reloadData()
+    }
+}
+
+
+// MARK: - TableView Delegate-DataSource
+extension HomeViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: Configure
+    func configureTableView() {
+        tableView.register(UINib(nibName: CharacterViewCell.cellIdentifier,
+                                      bundle: nil),
+                           forCellReuseIdentifier: CharacterViewCell.cellIdentifier)
+        
+        tableView.register(UINib(nibName: LoadingViewCell.cellIdentifier,
+                                 bundle: nil),
+                           forCellReuseIdentifier: LoadingViewCell.cellIdentifier)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK: TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell?
+        if(viewModel.areMoreCharacters && indexPath.row == viewModel.characters.count) {
+            cell = cellLoading(tableView, cellForItemAt: indexPath)
+        } else {
+            cell = cellCharacter(tableView, cellForItemAt: indexPath)
+        }
+        
+        loadMoreData(for: indexPath)
+        return cell ?? UITableViewCell()
+    }
+    
+    // MARK: Private
+    // MARK: Configure cells
+    private func cellLoading(_ tableView: UITableView, cellForItemAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: LoadingViewCell.cellIdentifier,
+                                      for: indexPath)
+    }
+    
+    private func cellCharacter(_ tableView: UITableView, cellForItemAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CharacterViewCell.cellIdentifier,
+                                                 for: indexPath) as? CharacterViewCell
+        
+        if let character = viewModel.character(by: indexPath.row) {
+            cell?.configure(character: character)
+        }
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    // MARK: Data functions
+    private func loadMoreData(for indexPath: IndexPath) {
+        if(!isLoadingCharacters && viewModel.areMoreCharacters && (viewModel.characters.count - indexPath.row) < HomeViewController.loadStartAt) {
+            isLoadingCharacters = true
+            viewModel.onUpdateData(withReset: false)
+        }
+    }
+}
--- rickandmortyapp/Features/Home/HomeViewModel.swift
+++ rickandmortyapp/Features/Home/HomeViewModel.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import RxSwift
+
+class HomeViewModel {
+    // MARK: - Observables
+    let needReloadData : PublishSubject<Bool> = PublishSubject()
+    
+    // MARK: - Properties
+    var infoList: InfoDAO?
+    var characters: [CharacterDAO] = []
+    
+    // MARK: - Computed properties
+    var areMoreCharacters: Bool {
+        return infoList?.nextUrl != nil
+    }
+    
+    var numberOfPage: Int {
+        return (characters.count / 20) + 1
+    }
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Public functions
+    func loadData() {
+        fetchCharacterList()
+    }
+    
+    func character(by index: Int) -> CharacterDAO? { characters[index] }
+    
+    func onUpdateData(withReset reset: Bool) {
+        if reset {
+            characters = []
+        }
+        
+        fetchCharacterList()
+    }
+    
+    // MARK: - Data functions
+    /// Get all layouts for home
+    fileprivate func fetchCharacterList() {
+        
+        let dataManager = DataManager.shared
+        dataManager.fetchCharacters(inPage: numberOfPage) { (result) in
+            switch result {
+            case .success(data: let data):
+                self.parseData(data)
+                self.needReloadData.onNext(true)
+                
+            case .failure(error: let error):
+                print(error)
+            }
+        }
+    }
+    
+    fileprivate func parseData(_ data: Any?) {
+        guard let charlist = data as? CharacterListDAO else { return }
+        
+        self.infoList = charlist.info
+        self.characters.append(contentsOf: charlist.characters)
+    }
+}
