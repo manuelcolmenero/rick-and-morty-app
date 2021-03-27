c31d7533c781ad10354f9ce70ffb03f938a928c9
Manuel Colmenero Navarro <mcolmenero@nubico.es>
2021-03-27 18:35:38 +0100

    Add DataManager

--- rickandmortyapp/Data/DataManager.swift
+++ rickandmortyapp/Data/DataManager.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+/// Class for managing local and remote data
+class DataManager {
+    // MARK: - SINGLETON implementation
+    // Object unique instance
+    static let shared = DataManager()
+    
+    // Initializer access level change now
+    private init() {}
+    
+    /// Call to get the list of characters
+    /// - Parameters:
+    ///   - page: Page number to request
+    ///   - callback: closure for return data
+    func fetchCharacters(inPage page: Int = 0, callback: @escaping ServiceCompletion) {
+        
+        let apiManager = ApiManager.shared
+        apiManager.fetchCharacters(inPage: page) { (result) in
+        
+            switch result {
+            case .success(data: let characterList):
+                
+                // Data validation
+                guard let listDTO = characterList as? CharacterListDTO else {
+                    callback(ServiceResult.failure(error: "Call without data. Contact us")) // TODO: Change harcode
+                    return
+                }
+                
+                // Mapper DTO to DAO
+                let listDAO = CharacterListDTOToDAOMapper().map(listDTO)
+                
+                // TODO: Find in userDefault if ID is Favorite
+                
+                // Return success result with characters
+                callback(ServiceResult.success(data: listDAO))
+                
+            case .failure(error: let error):
+                // return failure string
+                callback(ServiceResult.failure(error: error))
+            }
+        }
+    }
+}
