7642e34783da5012e82c2a461cfab47c2e835c5c
Manuel Colmenero Navarro <mcolmenero@nubico.es>
2021-03-27 11:10:22 +0100

    first commit

---
+++ rickandmortyapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickandmortyapp
+//
+//  Created by Manuel Colmenero Navaroo on 27/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
