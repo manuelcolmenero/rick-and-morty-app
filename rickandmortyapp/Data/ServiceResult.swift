326c882c0dc74b71b14f5a5cfc1992b59e19bacd
Manuel Colmenero Navarro <mcolmenero@nubico.es>
2021-03-27 18:34:39 +0100

    Add ServiceResult

--- rickandmortyapp/Data/ServiceResult.swift
+++ rickandmortyapp/Data/ServiceResult.swift
@@ -7,5 +7,11 @@
 
 import Foundation
 
+// Service result enum with data parameters
+enum ServiceResult {
+    case success(data: Any?)
+    case failure(error: String?)
+}
+
 // Define new type closure for return data
 typealias ServiceCompletion = (_ result: ServiceResult) -> ()
